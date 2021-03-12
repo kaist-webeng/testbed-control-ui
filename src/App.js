4b745e061a289b316ae08674105c2da508f312df
Sanghoon Kim <20144277+kim-sanghoon@users.noreply.github.com>
2021-03-12 19:48:43 +0900

    Use SWR for retrieving descriptions

--- src/App.js
+++ src/App.js
@@ -1,48 +1,38 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import axios from 'axios';
+import useSWR from 'swr';
 import { ToastProvider } from 'react-toast-notifications';
 
-import ControlArray from './ControlArray';
+import ControlArray from './components/ControlArray';
 
 function App() {
-  const [descriptions, setDescriptions] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const fetcher = url => axios.get(url).then(r => r.data);
+  const { data, error } = useSWR(
+    'http://server.seiker.kr:8000/api/services', fetcher
+  );
 
-  useEffect(() => {
-    const fetchDescriptions = async () => {
-      try {
-        setDescriptions(null);
-        setIsLoading(true);
-        setError(null);
-
-        // Retrieve descriptions from the registry server.
-        const response = await axios.get(
-          'http://server.seiker.kr:8000/api/services/'
-        );
-        setDescriptions(response.data);
-      } catch (e) {
-        setError(e);
-      }
-      setIsLoading(false);
-    };
-
-    fetchDescriptions();
-  }, []);
-
-  if (isLoading)
-    return <div className="loading vertical-center">Loading descriptions...</div>;
-  
   if (error)
-    return <div className="loading vertical-center" style={{color: "red"}}>Failed to retrieve descriptions.</div>;
+    return (
+      <div 
+        className="loading vertical-center" 
+        style={{color: "red"}}
+      >
+        Failed to retrieve descriptions.
+      </div>
+    );
 
-  if (!descriptions)
-    return <div>There is nothing to show...</div>;
+  if (!data)
+    return (
+      <div 
+        className="loading vertical-center"
+      >
+        Loading descriptions...
+      </div>
+    );
 
-  // Render the loaded data.
   return (
     <ToastProvider>
-      <ControlArray descriptions={descriptions} />
+      <ControlArray descriptions={data} />
     </ToastProvider>
   );
 }
